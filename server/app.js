d3b977828630660f9363152218dedb061cb906ab
Teschy <nlt5582@rit.edu>
2021-03-30 12:21:19 -0400

    Donomaker C Getting there.

--- server/app.js
+++ server/app.js
@@ -7,6 +7,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const redis = require('redis');
+const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -26,12 +30,29 @@ mongoose.connect(dbURL, mongooseOptions, (err) => {
   }
 });
 
+let redisURL = {
+  hostname: 'redis-14000.c257.us-east-1-3.ec2.cloud.redislabs.com',
+ port: 14000,
+};
+
+let redisPASS = 'TPfdDV5bRfzBjcKFbHbozoLGfwfxGMqL';
+if(process.env.REDISCLOUD_URL){
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  [, redisPass] = redisURL.auth.split(':');
+}
+let redisClient = redis.createClient({
+  host: redisURL.hostname,
+  port: redisURL.port,
+  password: redisPASS,
+});
+
 // Pull in our routes
 const router = require('./router.js');
 
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.disable('x-powered-by');
 app.use(compression());
 app.use(bodyParser.urlencoded({
   extended: true,
@@ -39,15 +60,33 @@ app.use(bodyParser.urlencoded({
 
 app.use(session({
   key: 'sessionid',
+  store: new RedisStore({
+    client: redisClient,
+  }),
   secret: 'Domo Arigato',
   resave: true,
   saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+  },
 }));
 
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
+
 app.use(cookieParser());
+
+//csrf must come AFTER app.use(cookieParser());
+//and app.use(SESSION({.....});
+//SHOULD COME BEFORE the router
+app.use(csrf());
+app.use((err, req, res, next) => {
+    if(err.code !== 'EBADCSRFTOKEN') return next(err);
+
+    console.log('Missing CSRF token!');
+    return false;
+});
 
 router(app);
 
